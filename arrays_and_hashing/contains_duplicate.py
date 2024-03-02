0a03a04cb9aa2ee7a65c9bf3f368ed3c3ff1e26f
AmannSingh <amann.singh7@outlook.com>
2024-03-02 01:31:06 -0800

    contains duplicate code

---
+++ arrays_and_hashing/contains_duplicate.py
@@ -0,0 +1,22 @@
+class Solution: 
+    def containsDuplicate(self, nums:list[int]) -> bool: 
+        for item in nums:
+               dict = {}
+        x = 0
+        for i in nums:
+                dict[i] = x
+                x+=1
+            
+            
+        if(len(dict) != len(nums)):
+            return True
+        else: 
+            return False
+        
+def main():
+    solution_object = Solution()
+    nums = [1,2,3,4,5,1,2]
+    print(solution_object.containsDuplicate(nums))
+
+if __name__ == '__main__':
+     main()
