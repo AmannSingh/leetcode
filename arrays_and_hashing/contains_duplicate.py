9d63662c14dd3fc1e455901545c564d275ec499a
AmannSingh <amann.singh7@outlook.com>
2024-05-27 17:36:59 -0700

    checking solution using dictionary

--- arrays_and_hashing/contains_duplicate.py
+++ arrays_and_hashing/contains_duplicate.py
@@ -1,22 +1,28 @@
 class Solution: 
     def containsDuplicate(self, nums:list[int]) -> bool: 
         dict = {}
-        for item in nums:
-               dict = {}
-        x = 0
+
+        #push items into dictionary and set it's value to 1 
         for i in nums:
-                dict[i] = x
-                x+=1
-            
-            
-        if(len(dict) != len(nums)):
-            return True
-        else: 
-            return False
+
+            # if the item is seen again increment its value by 1 
+            if i in dict:
+                dict[i] += 1
+            else:
+                dict[i] = 1 
+        
+        #check to see if an item showed up more than once in the nums list 
+        #by checking if value of the key is > 1 
+        for key in dict:
+            if(dict[key] > 1):
+                return True 
+        
+        return False 
+
         
 def main():
     solution_object = Solution()
-    nums = [1,2,3,4,5,1,2]
+    nums = [1,2,3,4,5]
     print(solution_object.containsDuplicate(nums))
 
 if __name__ == '__main__':
