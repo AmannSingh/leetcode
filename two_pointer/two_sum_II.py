be895ea93c601a64613a37082f773108dde191e9
AmannSingh <amann.singh7@outlook.com>
2024-09-23 22:42:52 -0700

    two_sum_II

---
+++ two_pointer/two_sum_II.py
@@ -0,0 +1,37 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        result = []
+        right = len(numbers) - 1
+        left = 0
+
+        numbers.sort()
+
+        while left < right:
+
+            sum = numbers[right] + numbers[left]
+
+            if sum == target:
+                result.append(left+1)
+                result.append(right+1)
+
+                return result
+
+            if sum < target:
+                left += 1
+
+            elif sum > target:
+                right -= 1
+
+        return result
+
+def main():
+    solution_object = Solution()
+
+    numbers = [1, 2, 10, 4, 15, 7, 8, 12, 11]
+    target = 17
+    print(solution_object.twoSum(numbers, target))
+
+
+if __name__ == "__main__":
+
+    main()
